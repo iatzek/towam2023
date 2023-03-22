642b3108707c71478dc1d581b5b228d600dc68d0
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-22 13:49:25 +0100

    Wersja PoC

--- TOWAMRK/Program.cs
+++ TOWAMRK/Program.cs
@@ -6,7 +6,19 @@ namespace TOWAMRK
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Równania kwadratowe");
+
+            double a = 1;
+            double b = 0;
+            double c = -9;
+
+
+            double delta = b * b - 4 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("x1={0}, x2={1}", x1, x2);
+
         }
     }
 }
