ab4f0f59061e11e73cb4e189bf89efc7a8d2c0b2
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-22 13:58:24 +0100

    wersja z rozbitym przypadkiem delta==0

--- TOWAMRK/Program.cs
+++ TOWAMRK/Program.cs
@@ -21,14 +21,21 @@ namespace TOWAMRK
 
             double delta = b * b - 4 * a * c;
 
-            if (delta >= 0)
+            if (delta > 0)
             {
 
                 double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
                 double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
 
                 Console.WriteLine("x1={0}, x2={1}", x1, x2);
-            } else
+            } else if (delta == 0)
+            {
+                double x = (-b ) / 2 * a;
+
+                Console.WriteLine("podwojny pierwiastek x={0}", x);
+
+            }
+            else
             {
                 Console.WriteLine("brak rozwiazan");
             }
