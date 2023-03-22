bc386629d19f0faea1ef20b5c934404cbdd41d4e
Jacek Kawa <jacek.kawa@polsl.pl>
2023-03-22 13:51:10 +0100

    sprawdz nieujemnosc delty

--- TOWAMRK/Program.cs
+++ TOWAMRK/Program.cs
@@ -14,11 +14,18 @@ namespace TOWAMRK
 
 
             double delta = b * b - 4 * a * c;
-            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
 
-            Console.WriteLine("x1={0}, x2={1}", x1, x2);
+            if (delta >= 0)
+            {
 
+                double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+                Console.WriteLine("x1={0}, x2={1}", x1, x2);
+            } else
+            {
+                Console.WriteLine("brak rozwiazan");
+            }
         }
     }
 }
